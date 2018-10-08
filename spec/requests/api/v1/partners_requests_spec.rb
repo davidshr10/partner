56489d926bc092bb41380ed75b3f4573530ac417
davidshr10 <1936038+davidshr10@users.noreply.github.com>
2018-10-08 19:41:45 -0400

    Allow users to follow email link and register in the app

--- spec/requests/api/v1/partners_requests_spec.rb
+++ spec/requests/api/v1/partners_requests_spec.rb
@@ -17,18 +17,21 @@ describe "Partners Api Requests" do
 
   describe 'POST "/api/v1/partners' do
     it "creates a partner" do
-      partner = FactoryBot.build(:partner)
-
       attrs = {
         diaper_bank_id: 1,
         diaper_partner_id: 1,
-        email: partner.email
+        email: "partner@email.com"
       }
 
       post api_v1_partners_path, params: attrs
 
       expect(response).to have_http_status(:ok)
       expect(Partner.count).to eq 1
+      expect(Partner.last.email).to eq("partner@email.com")
+
+      last_email = ActionMailer::Base.deliveries.last
+      expect(last_email.to).to include "partner@email.com"
+      expect(last_email.html_part.body.to_s).to include "Accept invitation"
     end
   end
 end
